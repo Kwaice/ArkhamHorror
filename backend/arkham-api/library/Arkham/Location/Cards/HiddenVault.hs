f6fbb398fc7933e9ccab75cfbdf3df6da463082a
Kwaice <25953788+Kwaice@users.noreply.github.com>
2026-08-13 08:56:43 +0200

    Update HiddenVault.hs
    Not sure about the Anywhere here
    
    Basically the forced ability doesn't trigger when Hidden Vault is put into play

--- backend/arkham-api/library/Arkham/Location/Cards/HiddenVault.hs
+++ backend/arkham-api/library/Arkham/Location/Cards/HiddenVault.hs
@@ -14,12 +14,12 @@ newtype HiddenVault = HiddenVault LocationAttrs
 
 hiddenVault :: LocationCard HiddenVault
 hiddenVault =
-  locationWith HiddenVault Cards.hiddenVault 3 (Static 1) (canBeFlippedL .~ True)
+  locationWith HiddenVault Cards.hiddenVault 3 (PerPlayer 1) (canBeFlippedL .~ True)
 
 instance HasAbilities HiddenVault where
   getAbilities (HiddenVault a) =
     extendRevealed a
-      [ mkAbility a 1 $ forced $ RevealLocation #after Anyone (be a)
+      [ mkAbility a 1 $ forced $ RevealLocation #after Anyone Anywhere
       , restricted a 2 Here
           $ freeReaction
           $ SkillTestResult #after You (WhileEvadingAnEnemy $ enemyAt a) (SuccessResult $ atLeast 2)
